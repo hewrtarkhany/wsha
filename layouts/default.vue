58086ffa607c0f8e1d33d19e79f788a2b7b6d2d6
Hewr Tarkhany <46586350+hewrtarkhany@users.noreply.github.com>
2022-04-05 23:17:58 -0700

    added a new page test

--- layouts/default.vue
+++ layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+         {
+          icon: 'mdi-screwdriver',
+          title: 'test',
+          to: '/test',
+        },
       ],
       miniVariant: false,
       right: true,
---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row>
+<p>hello</p>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'test',
+}
+</script>
